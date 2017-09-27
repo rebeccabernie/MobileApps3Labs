f6f897156eb1988c0db4b50a53b882d58bd1cd71
Rebecca <rebecca.kane@outlook.com>
2017-09-27 11:49:47 +0100

    Working on calculations (arg1 & arg2)

--- 03_ElementBinding/03_ElementBinding/MainPage.xaml.cs
+++ 03_ElementBinding/03_ElementBinding/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,5 +27,39 @@ namespace _03_ElementBinding
         {
             this.InitializeComponent();
         }
+    }
+
+    public class Adder : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int Arg1 {
+            get { return arg1; }
+            set
+            {
+                arg1 = value;
+                if (PropertyChanged != null)
+                {
+                    // Raise the property changed event handler - calls update method on data bound values, answer value gets current value
+                    PropertyChanged(this, new PropertyChangedEventArgs("AnswerValue")); // Answer Value is changed
+                }
+            }
+        }
+        private int arg1;
+        public int Arg2 {
+            get { return arg2; }
+            set
+            {
+                arg2 = value;
+                if (PropertyChanged != null)
+                {
+                    // Raise the property changed event handler - calls update method on data bound values, answer value gets current value
+                    PropertyChanged(this, new PropertyChangedEventArgs("AnswerValue")); // Answer Value is changed
+                }
+            }
+        }
+        private int arg2;
+
+        public int AnswerValue { get { return arg1 + arg2; } }
+
     }
 }
